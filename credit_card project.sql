a9282a88d3ddf7c8d343ceaa15f0ba0d79e3ba98
demi-ii <obademilade.fadipe@gmail.com>
2025-09-02 10:30:19 -0500

    Add files via upload
    added story to tableau dashboard

--- credit_card project.sql
+++ credit_card project.sql
@@ -49,7 +49,7 @@ FROM credit_card
  GROUP BY relationship_status
  ORDER BY default_rate DESC;
 
-##What is the most risky age group ?
+##What is the most risky age group , higher severity?
 SELECT 
 CASE
 	WHEN age BETWEEN 18 AND 29 THEN '18-29'
@@ -76,6 +76,37 @@ FROM  credit_card
 GROUP BY limit_range
 oRDER BY default_rate DESC;
 
+# Number of clients in each limit range group to explain severity further
+SELECT 
+ CASE
+ WHEN age BETWEEN 18 AND 29 THEN '18-29'
+ WHEN age BETWEEN 30 AND 39 THEN '30-39'
+ ELSE '40+'
+    END AS age_group,
+CASE 
+WHEN LIMIT_BAL < 50000 THEN '<50K'
+WHEN LIMIT_BAL < 100000 THEN '50K-100K'
+WHEN LIMIT_BAL < 200000 THEN '100K-200K'
+WHEN LIMIT_BAL < 500000 THEN '200K-500K'
+ELSE '500K+' 
+    END AS limit_range,
+COUNT(*) AS 'num_client'
+FROM credit_card
+GROUP BY age_group,limit_range
+ORDER BY num_client DESC;
+
+## segmentation of clients risk level
+SELECT 
+ROUND((SUM(`default.payment.next.month`)/COUNT(*) )*100,2) AS default_rate,
+CASE
+WHEN (PAY_0 + PAY_2 + PAY_3 + PAY_4 + PAY_5 + PAY_6) < '0' THEN 'low risk'
+WHEN (PAY_0 + PAY_2 + PAY_3 + PAY_4 + PAY_5 + PAY_6)  BETWEEN 1 AND 5 THEN 'medium risk'
+ELSE 'high risk'
+END AS Risk_status,
+COUNT(*) AS num_clients
+FROM credit_card
+GROUP BY Risk_status
+ORDER BY default_rate DESC;
 
 ###. Which groups contribute the most to overall risk? age_group, gender, education_status and relationship_status?
  SELECT ROUND((SUM(`default.payment.next.month`)/COUNT(*) )*100,2) AS default_rate,
@@ -105,15 +136,3 @@ FROM credit_card
 GROUP BY age_group,gender,education_status,relationship_status
 ORDER BY default_rate DESC;
 
-## segmentation of clients risk level
-SELECT 
-ROUND((SUM(`default.payment.next.month`)/COUNT(*) )*100,2) AS default_rate,
-CASE
-WHEN (PAY_0 + PAY_2 + PAY_3 + PAY_4 + PAY_5 + PAY_6) < '0' THEN 'low risk'
-WHEN (PAY_0 + PAY_2 + PAY_3 + PAY_4 + PAY_5 + PAY_6)  BETWEEN 1 AND 5 THEN 'medium risk'
-ELSE 'high risk'
-END AS Risk_status,
-COUNT(*) AS num_clients
-FROM credit_card
-GROUP BY Risk_status
-ORDER BY default_rate DESC;
